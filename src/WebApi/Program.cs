b99086cae3fca542d1046fe1a0d9640cdb8147fd
Luis Pool <pool.luis@gmail.com>
2022-02-01 00:08:44 -0600

    Seeding The Database

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using WebApi.Context;
+using WebApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +12,25 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Seeding the database.
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try
+    {
+        // Seed default users
+        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+        var rolManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        await ApplicationDbContextSeed.SeedEssentialsAsync(userManager, rolManager);
+    }
+    catch (Exception ex)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "An error ocurred seeding the DB.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
