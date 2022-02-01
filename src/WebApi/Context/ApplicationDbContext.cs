ab2106b8da4277fb2dfb63b1c43b386321c1258a
Luis Pool <pool.luis@gmail.com>
2022-01-31 23:41:38 -0600

    Adding DbContext

---
+++ src/WebApi/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
