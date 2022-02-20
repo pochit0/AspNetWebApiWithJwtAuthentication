1107f4ea69a07974ae105579ef84c88f03c43fea
Luis Pool <pool.luis@gmail.com>
2022-02-19 23:27:36 -0600

    User Service

---
+++ src/WebApi/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using WebApi.Models;
+using WebApi.Settings;
+
+namespace WebApi.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly JWT _jwt;
+
+        public UserService(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IOptions<JWT> jwt)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _jwt = jwt.Value;
+        }
+    }
+}
