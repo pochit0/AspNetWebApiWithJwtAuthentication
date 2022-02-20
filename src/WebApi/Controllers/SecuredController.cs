a12fc954383a70b6bdac03d4dcef775c232b8c5a
Luis Pool <pool.luis@gmail.com>
2022-02-19 23:23:05 -0600

    Secured Controller

---
+++ src/WebApi/Controllers/SecuredController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecuredController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetSecuredData()
+        {
+            return Ok("This Secured Data is available only for Authenticated Users.");
+        }
+    }
+}
