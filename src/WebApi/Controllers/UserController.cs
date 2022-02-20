d80a602230a481a3660b811c776c7e50e51a31ea
Luis Pool <pool.luis@gmail.com>
2022-02-19 23:30:01 -0600

    User Controller

---
+++ src/WebApi/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+    }
+}
